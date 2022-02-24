4318e2b672a26fb108eb57fed2fe64cce875bc28
Dima <99317239+8dimas8@users.noreply.github.com>
2022-02-24 12:08:55 +0200

    Update Third_task.java

--- src/com/company/Third_task.java
+++ src/com/company/Third_task.java
@@ -3,12 +3,12 @@ package com.company;
 public class Third_task {
     public static void main(String[] args) {
         double a,b,c,d,e;
-        a=14.44;
-        b=a-a%10;
-        c=(a-b)-(a-b)%1;
-        d=(a-b)%1;
-        e=b+c;
-        int f =(int)e;
+        a=16.4;
+        b=a+0.5;
+        c=b-(b%10);//10;
+        d=(b-c)-((b-c)%1);
+        e=c+d;
+        int f=(int) e;
         System.out.println("Число="+a);
         System.out.println("Заокруглене число="+(f));
 
